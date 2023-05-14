90d798cc004529431093a524b9a08757e0d376f6
MuhammetGunes <muhammett-gunes@hotmail.com>
2023-05-14 13:44:04 +0300

    Merkezi hata yönetimi kodları düzeltildi.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -79,10 +79,9 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.ConfigureCustomExceptionMiddleware();
             app.UseCors(builder => builder.WithOrigins("http://localhost:3000", "http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
-
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
